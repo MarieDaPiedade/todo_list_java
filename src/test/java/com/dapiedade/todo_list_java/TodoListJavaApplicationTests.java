129981420d3c66fbf5d80dd7c0fcaeedf6a7f15e
MarieDaPiedade <marie@dapiedade.org>
2022-07-05 11:01:21 +0200

    first commit

---
+++ src/test/java/com/dapiedade/todo_list_java/TodoListJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dapiedade.todo_list_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
